0e8dc29e172fdd75f23ea16a242497b4645ca9d9
Eric Santos <eric355402@yahoo.com>
2022-02-12 14:56:09 -0700

    fixed error on exporting img array

--- src/images.js
+++ src/images.js
@@ -3,4 +3,5 @@ import img2 from './assets/img2.jpg';
 import img3 from './assets/img3.jpg';
 import img4 from './assets/img4.jpg';
 
-export default [img1, img2, img3, img4];
+let images = [img1, img2, img3, img4];
+export default images;
